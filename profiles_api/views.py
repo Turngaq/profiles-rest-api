895cfa3342491534cb196a53ace90fceddda5a17
Turngaq <ssirey30zero@gmail.com>
2022-02-01 14:56:38 +0300

    rest api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     # Test API view
+    serializer_class = serializers.HelloSerializer
 
-   def get(self, request, format=None):
+    def get(self, request, format=None):
         # Returns a list of API features
         an_apiview = [
             'Users HTTP methods as function (get, post, patch, put, delete)',
@@ -13,6 +17,34 @@ class HelloApiView(APIView):
             'Gives you the most control over you application logic',
             'Is mapped manually to URLs',
         ] 
-        something = "SOOOO"
 
-        return Response({'message': 'Hello!', 'an_apiview': an_apiview, 'something': something})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+
+    def post(self, request):
+        # Create a hello message with our name
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        # Handle updating an object
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        # Handle particulary update for an object
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        # Delete an object
+        return Response({'method': 'DELETE'})
+
+
